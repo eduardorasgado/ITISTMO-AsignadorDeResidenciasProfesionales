073e8937b2374ddda82aa9c0519ee5380d675f0f
lal <eduardo.rasgado@hotmail.com>
2018-08-24 20:29:52 -0500

    fillable in User model

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'cargo', 'disponibilidad', 'num_asignaciones', 'anteproyecto_cuenta', 'proyecto_cuenta', 'num_control', 'telefono', 'password',
     ];
 
     /**
