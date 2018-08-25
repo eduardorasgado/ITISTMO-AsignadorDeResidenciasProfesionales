c63a0050c04191141dfffec7bdc6d8945dcb8558
lal <eduardo.rasgado@hotmail.com>
2018-08-25 04:03:54 -0500

    new sinodalia create in createNewAssignment

--- app/Http/Controllers/SinodaliaController.php
+++ app/Http/Controllers/SinodaliaController.php
@@ -8,6 +8,19 @@ class SinodaliaController extends Controller
 {
     public function create(Request $request)
     {
-    	return 'sinodalia ha sido creada';
+    	// creando la sinodalia
+    	$createdSinodalia = $request->user()->sinodalia()->create([
+    			'residente' => $request->residente,
+    			'carrera' => $request->carrera,
+    			'num_control' => $request->num_control,
+    			'proyecto' => $request->proyecto,
+    			'id_secretario' => $request->secretario,
+    			'id_vocal' => $request->vocal,
+    			'id_vocal_sup' => $request->vocalSuplente,
+    			'aprobacion' => 0,
+    	]);
+
+    	// regresar una respuesta
+    	return 'Sinodalia creada';
     }
 }
--- app/User.php
+++ app/User.php
@@ -38,6 +38,6 @@ class User extends Authenticatable
 
     public function sinodalia() 
     {
-        return $this->hasMany(Sinodalias::class);
+        return $this->hasMany(Sinodalia::class);
     }
 }
