e6e422b7833db0bf74fa4bf257abe4c06c96fb49
lal <eduardo.rasgado@hotmail.com>
2018-09-06 18:40:51 -0500

    style to maestros blade

--- resources/views/teachers/teacherList.blade.php
+++ resources/views/teachers/teacherList.blade.php
@@ -4,24 +4,28 @@
     <div class="container">
     	<h2 class="text-center">Profesorado perteneciente a la academia</h2>
     	<br>
+    	<hr>
     	@foreach($users as $user)
-	    	<div class="jumbotron" style="background: #333333; color: white; padding: 20px;">
+	    	<div class="" style="padding: 20px; font-size: 1.2em">
 	    		
 	    		<div class="row justify-content-md-center">
 	    			<div class="col-md-6">
-	    				<h4 class="text-center">{{ $user->name }}</h4>
-	    				<b>Número de Control: </b><p>{{ $user->num_control }}</p>
-	    				<b>Número de sinodalías registradas y activas:</b><p>{{$user->num_asignaciones}} sinodalía(s)</p>
-	    				<b>Telefono celular: </b><p>{{ $user->telefono }}</p>
+	    				<h3 class="alert alert-dark">{{ $user->name }}</h3>
+	    				<div class="alert alert-primary">
+	    					<b>Número de Control: </b><p>{{ $user->num_control }}</p>
+		    				<b>Número de sinodalías registradas y activas:</b><p>{{$user->num_asignaciones}} sinodalía(s)</p>
+		    				<b>Telefono celular: </b><p>{{ $user->telefono }}</p>
+	    				</div>
 	    			</div>
 	    			<div class="col-md-6">
 	    				
 	    				<br><br><br>
-	    				<a style="margin-right: 20px;" class="btn btn-light" href="">Editar</a>
-	    				<a class="btn btn-light" href="">Eliminar</a>
+	    				<a style="margin-right: 20px;" class="btn btn-primary" href="">Editar</a>
+	    				<a class="btn btn-danger" href="">Eliminar</a>
 	    			</div>
 	    		</div>
 	    	</div>
+	    	<hr>
 	    @endforeach
     </div>
 @endsection
