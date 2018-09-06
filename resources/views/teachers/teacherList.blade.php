611bb3fcf7794b16435fd5054adc5848a51f10d4
lal <eduardo.rasgado@hotmail.com>
2018-09-06 17:46:22 -0500

    style to teacher list blade

--- resources/views/teachers/teacherList.blade.php
+++ resources/views/teachers/teacherList.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    {{ $users }}
+    <div class="container">
+    	@foreach($users as $user)
+	    	<div class="jumbotron" style="">
+	    		<h4>{{ $user->name }}</h4>
+	    	</div>
+	    @endforeach
+    </div>
 @endsection
