20d319993448a703ec21067ba95b9a4b3f199e05
lal <eduardo.rasgado@hotmail.com>
2018-09-06 19:00:40 -0500

    styling to teacherListing blade

--- resources/views/teachers/teacherList.blade.php
+++ resources/views/teachers/teacherList.blade.php
@@ -3,29 +3,36 @@
 @section('content')
     <div class="container">
     	<h2 class="text-center">Profesorado perteneciente a la academia</h2>
+    	<a class="btn btn-primary" href="{{ URL::previous() != url()->current() ? URL::previous() : "/home" }}">Atrás</a>
     	<br>
     	<hr>
+    	<div class="row justify-content-md-center">
     	@foreach($users as $user)
-	    	<div class="" style="padding: 20px; font-size: 1.2em">
-	    		
-	    		<div class="row justify-content-md-center">
-	    			<div class="col-md-6">
-	    				<h3 class="alert alert-dark">{{ $user->name }}</h3>
-	    				<div class="alert alert-primary">
-	    					<b>Número de Control: </b><p>{{ $user->num_control }}</p>
-		    				<b>Número de sinodalías registradas y activas:</b><p>{{$user->num_asignaciones}} sinodalía(s)</p>
-		    				<b>Telefono celular: </b><p>{{ $user->telefono }}</p>
-	    				</div>
-	    			</div>
-	    			<div class="col-md-6">
-	    				
-	    				<br><br><br>
-	    				<a style="margin-right: 20px;" class="btn btn-primary" href="">Editar</a>
-	    				<a class="btn btn-danger" href="">Eliminar</a>
-	    			</div>
-	    		</div>
-	    	</div>
-	    	<hr>
+  			<div class="col-md-6" style="padding: 20px; font-size: 1.2em">
+
+  				<h3 class="alert alert-dark">{{ $user->name }} 
+  					<a style="margin-left: 20px;
+  										margin-right: 20px;" 
+  										class="btn btn-primary" href="">Editar</a>
+  					<a class="btn btn-danger" href="">Eliminar</a>
+  				</h3>
+
+  				<div class="alert alert-primary">
+  					<b>Número de Control: </b><p>{{ $user->num_control }}</p>
+    				<b>Número de sinodalías registradas y activas:</b><p>{{$user->num_asignaciones}} sinodalía(s)</p>
+    				<b>Telefono celular: </b><p>{{ $user->telefono }}</p>
+    				<b>Cargo: </b>
+    					@if($user->cargo == 0)
+    						<p>Asignador</p>
+    					@elseif($user->cargo == 1)
+    						<p>Administrativo</p>
+    					@elseif($user->cargo == 2)
+    						<p>Profesorado</p>
+    					@endif
+  				</div>
+  				
+  			</div>    			  
 	    @endforeach
+	    </div>
     </div>
 @endsection
